40c33a1bb188524be55ce8bbc202c5c8f86de656
Posh <posh@Mac.fritz.box>
2026-03-23 19:53:09 +0100

    Delete function added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from data_models import db, Author, Book
 # 1. Instanz
 app = Flask(__name__)
 
-#2
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 
@@ -15,26 +15,20 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    # 1. Parameter aus der URL abgreifen
     search_query = request.args.get('search')
-    sort_by = request.args.get('sort', 'title') # Standard ist 'title'
+    sort_by = request.args.get('sort', 'title')
 
-    # 2. Basis-Abfrage vorbereiten
     query = Book.query
 
-    # 3. Filter anwenden (Step 5)
     if search_query:
         query = query.filter(Book.title.ilike(f'%{search_query}%'))
 
-    # 4. Sortierung anwenden (Das fehlt bei dir noch!)
+
     if sort_by == 'author':
-        # Join wird benötigt, um auf Author.name zuzugreifen
         query = query.join(Author).order_by(Author.name)
     else:
-        # Sortierung nach Buchtitel
         query = query.order_by(Book.title)
 
-    # 5. Jetzt erst die fertige Query ausführen
     all_books = query.all()
 
     return render_template('home.html', books=all_books)
@@ -50,7 +44,7 @@ def add_author():
 
         # 'db.session' ist die aktive Verbindung zur library.sqlite
         db.session.add(new_author)
-        db.session.commit()  # Erst hier werden die Daten physisch gespeichert
+        db.session.commit()
 
         return "Author successfully added!"
 
@@ -60,11 +54,10 @@ def add_author():
 @app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
-        # Daten für das Buch aus dem Formular ziehen
         isbn = request.form.get('isbn')
         title = request.form.get('title')
         year = request.form.get('publication_year')
-        author_id = request.form.get('author_id')  # Das ist die ID vom ausgewählten Autor
+        author_id = request.form.get('author_id')
 
         # Neues Buch-Objekt erstellen
         new_book = Book(isbn=isbn, title=title, publication_year=year, author_id=author_id)
@@ -81,8 +74,28 @@ def add_book():
 #with app.app_context():
     #db.create_all()
 
+from flask import Flask, render_template, request, redirect
+
+
+@app.route('/delete_book/<id>')
+def delete_book(id):
+    # 1. Das Buch finden
+    book = Book.query.get(id)
+    author = book.author
+    # 2. Buch löschen
+    db.session.delete(book)
+    db.session.commit()
+
+
+    if len(author.books) == 0:
+        db.session.add(author)
+        db.session.delete(author)
+        db.session.commit()
+
+    return redirect('/')
+
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Das hier erstellt die Tabellen, falls sie fehlen
+        db.create_all()
     app.run(debug=True, port=5000)
 
