2e22ef49d195cff7973a1e8cad0c4547a0d7f6a0
Posh <posh@Mac.fritz.box>
2026-03-23 11:53:44 +0100

    Step 2: Flask-SQLAlchemy configuration and URI setup complete

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
 
+# 1. Instanz
+app = Flask(__name__)
+
+#2
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+
+db.init_app(app)
